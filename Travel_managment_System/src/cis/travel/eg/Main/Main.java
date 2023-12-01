0878062522784ec283ada6d5c3a3b3a31801085f
Nouran haitham Othman <115147678+niro12-coder@users.noreply.github.com>
2023-12-01 21:48:37 +0200

    Update Main.java

--- Travel_managment_System/src/cis/travel/eg/Main/Main.java
+++ Travel_managment_System/src/cis/travel/eg/Main/Main.java
@@ -1,12 +1,7 @@
 package cis.travel.eg.Main;
 
-import cis.travel.eg.User.Customer;
-
-import java.util.ArrayList;
-
 public class Main {
-    public static void main(String[] args) {
 
 
-    }
+    
 }
