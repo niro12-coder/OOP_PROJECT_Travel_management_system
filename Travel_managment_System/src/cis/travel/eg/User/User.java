519ba8691712ab8d6eeebc9b7a5d080b22a7e069
mariamgharib553@gmail.com <mariamgharib553>
2023-12-05 00:06:40 +0200

    complete

--- Travel_managment_System/src/cis/travel/eg/User/User.java
+++ Travel_managment_System/src/cis/travel/eg/User/User.java
@@ -1,5 +1,6 @@
 package cis.travel.eg.User;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import cis.travel.eg.Main.Main;
 import cis.travel.eg.Trip.Trip;
@@ -11,6 +12,7 @@ abstract public class User {
     private String Password;
     private String PhoneNumber;
     private String Email;
+    private char Gender;
     private int Age;
 
     public String getUsername() {
@@ -23,8 +25,6 @@ abstract public class User {
         //      System.out.println(" ");
         //      this.setUsername(username, sizeOfArray);
         //   }
-        //
-        System.out.println(Main.ANSI_COLORS[0] + " kkk");
         Username = username;
     }
 
@@ -52,7 +52,7 @@ abstract public class User {
         // cin_password
         if (newPassword == null || confirmPassword == null || newPassword.isEmpty() || confirmPassword.isEmpty())
         {
-            System.out.println(" Invalid Password  \n Enter valid password : "); // null
+            System.out.println(Main.ANSI_COLORS[2] + " Invalid Password  \n "+ Main.ANSI_COLORS[0] + "Enter valid password : "); // null
             return false;
         }
 
@@ -64,17 +64,17 @@ abstract public class User {
         boolean upperRule = !newPassword.equals(newPassword.toLowerCase());
         boolean lowerRule = !newPassword.equals(newPassword.toUpperCase());
         boolean numCheck = newPassword.matches("(.*)[0-9](.*)");
-        boolean symbolsRule = newPassword.matches("(.*)'#'(.*)") ; // '*', '_', '-'
+        boolean symbolsRule = newPassword.matches("(.*)#(.*)") || newPassword.matches("(.*)-(.*)")  || newPassword.matches("(.*)_(.*)") ; // '#', '_', '-'
 
         rule_count = ((upperRule ? 1 : 0) + (lowerRule ? 1 : 0) + (symbolsRule ? 1 : 0) + (numCheck ? 1 : 0));
 
 
         if(!equalCheck) {
-            System.out.println("Passwords must match \n");
+            System.out.println(Main.ANSI_COLORS[2] + "Passwords must match \n" + Main.ANSI_COLORS[0]);
             return false;
         }
         else if(!sizeCheck || rule_count < 4 || !numCheck){
-            System.out.println("Password doesn't follow instructions \n"); //Password must be at least 8 and #, *
+            System.out.println(Main.ANSI_COLORS[2] + "Password doesn't follow instructions \n" + Main.ANSI_COLORS[0]); //Password must be at least 8 and #, *
             return false;
         }
 
@@ -97,12 +97,8 @@ abstract public class User {
         boolean sizeCheck = phoneNumber.length() == 11;
         boolean numCheck = phoneNumber.matches("(.*)[0-9](.*)");
         boolean symbolsRule = !phoneNumber.matches("(.*)'#'(.*)") ; // '*', '_', '-'
-        boolean Check = false;
-        if(phoneNumber.matches("011(.*)") || phoneNumber.matches("010(.*)")
-                || phoneNumber.matches("015(.*)") || phoneNumber.matches("012(.*)") ) {
-            Check = true;
-        }
-
+        boolean Check = phoneNumber.matches("011(.*)") || phoneNumber.matches("010(.*)")
+                || phoneNumber.matches("015(.*)") || phoneNumber.matches("012(.*)");
 
         rule_count = ((sizeCheck ? 1 : 0) + (numCheck ? 1 : 0) + (symbolsRule ? 1 : 0) + (Check ? 1 : 0));
 
@@ -130,23 +126,46 @@ abstract public class User {
         return false;
     }
 
+    public char getGender() {
+        return Gender;
+    }
+
+    public void setGender(char gender) {
+        Gender = gender;
+    }
+
     public int getAge() {
         return Age;
     }
 
     public boolean setAge(int age) {
         if(age > 100 || age < 0) {
-            System.out.println("Invalid input");
+            System.out.println(Main.ANSI_COLORS[2] + "Invalid input" + Main.ANSI_COLORS[0]);
             return false;
         }
         Age = age;
         return true;
     }
 
+    ///////Abstract Methods///////
     abstract public void Register(ArrayList<Admin> admin, ArrayList<Customer> customer, ArrayList<Manager> manager, ArrayList<TourGuide> tourguide);
     abstract public boolean HomePage(ArrayList<Admin> Admins, ArrayList<Customer> Customers, ArrayList<TourGuide> TourGuides, ArrayList<Manager> Managers,ArrayList<Trip> Trips_system);
-    abstract public void Display_Profile();
-    abstract public void Edit_Profile();
+    abstract public int Display_Profile(ArrayList<Admin> admin, ArrayList<Customer> customer, ArrayList<Manager> manager, ArrayList<TourGuide> tourguide);
+    abstract public int Edit_Profile(ArrayList<Admin> admin, ArrayList<Customer> customer, ArrayList<Manager> manager, ArrayList<TourGuide> tourguide);
 
 
 }
+/* public static final String[] ANSI_COLORS = {
+            "\u001B[0m",    // Reset  Main.ANSI_COLORS[o]
+            "\u001B[30m",   // Black  Main.ANSI_COLORS[1]
+            "\u001B[31m",   // Red    Main.ANSI_COLORS[2]
+            "\u001B[32m",   // Green  Main.ANSI_COLORS[3]
+            "\u001B[33m",   // Yellow Main.ANSI_COLORS[4]
+            "\u001B[34m",   // Blue   Main.ANSI_COLORS[5]
+            "\u001B[35m",   // Purple Main.ANSI_COLORS[6]
+            "\u001B[36m",   // Cyan   Main.ANSI_COLORS[7]
+            "\u001B[37m"    // White  Main.ANSI_COLORS[8]
+
+    };*/
+
+
